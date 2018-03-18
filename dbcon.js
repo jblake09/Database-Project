580bb087e687291683a326853c111f6a2b66a7d4
Jeffrey E Blake Jr <jeffrey.e.blake@gmail.com>
2018-03-18 00:04:09 -0500

    EDIT

--- dbcon.js
+++ dbcon.js
@@ -3,7 +3,7 @@ var pool = mysql.createPool({
   connectionLimit : 10,
   host            : 'classmysql.engr.oregonstate.edu',
   user            : 'cs340_blakejef',
-  password        : '0586',
+  password        : '****',
   database        : 'cs340_blakejef'
 });
 module.exports.pool = pool;
